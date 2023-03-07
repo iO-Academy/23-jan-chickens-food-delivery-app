3f4b30dc220c2a1185e8b4553231e0e231dec1c6
Dan Abs <danabsolonefl@gmail.com>
2023-03-07 17:00:11 +0000

    responding to feedback

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,16 +1,16 @@
 import './Main.scss'
 import burgerImage from './burgers.webp'
 import Navbar from '../Navbar/Navbar'
-import BannerBoxFoodDelivered from '../BannerBoxFoodDelivered/BannerBoxFoodDelivered'
+import Title from '../Title/Title'
 
 function Main() {
   return (
-    <main className="Main">
+    <main>
       <Navbar />
-      <div style={{backgroundImage: "url('" + burgerImage + "')"}} className="burger-image m-md-3">
+      <div style={{backgroundImage: "url('" + burgerImage + "')"}} className="container-fluid burger-image m-md-3">
         <div className="row justify-content-center align-items-center h-100 m-3">
-          <div className="col-sm-12 col-md-5 semi-transparent bg-light h-50 flex-row justify-content-center align-items-center d-flex m-0">
-            <BannerBoxFoodDelivered />
+          <div className="col-sm-12 col-md-6 semi-transparent bg-light h-50 flex-row justify-content-center align-items-center d-flex m-0">
+            <Title />
           </div>
         </div>
       </div>
