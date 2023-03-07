16a44cb8e6e9d2495b989a9cff83005fbd844aa4
Dan Abs <danabsolonefl@gmail.com>
2023-03-07 14:53:06 +0000

    functional now need to tidy and wave

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -4,22 +4,18 @@ import burgerImage from './burgers.webp'
 function Main() {
   return (
     <main className="Main">
-      <nav className="navbar navbar-expand border-bottom shadow-sm ps-4">
-        <a href="#" className="navbar-brand fw-semibold text-sm-center">
+      <nav className="navbar navbar-expand border-bottom shadow-sm px-4 justify-content-md-start justify-content-center">
+        <a href="#" className="navbar-brand fw-semibold m-0">
           <span className="text-info">Food</span>
           <span>Delivery</span>
         </a>
       </nav>
       <div style={{
-        height: "400px", 
         backgroundImage: "url('" + burgerImage + "')",
-        backgroundSize: "100%",
-        backgroundPosition: "0% 20%",
-        backgroundRepeat: "no-repeat"
-        }} className="m-md-3">
+        }} className="burger-image m-md-3">
           <div className="row justify-content-center align-items-center h-100 m-3">
             <div className="col-sm-12 col-md-5 semi-transparent bg-light h-50 flex-row justify-content-center align-items-center d-flex m-0">
-              <div>
+              <div className="bannerTextBox">
               <h2 className="text-info fs-1 align-middle">Food. Delivered.</h2>
               <p className="align-middle fs-5 mb-0">Order your favourite food from local restaurants, right to your door.</p>
               </div>
