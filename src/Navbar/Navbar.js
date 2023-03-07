4bbca1d7c14b3b26abd3575c340a83c063e9082d
tauroid <adamjacobs173@googlemail.com>
2023-03-07 15:41:12 +0000

    Resolve WAVE through bolding

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,7 +1,7 @@
 function Navbar() {
   return (
     <nav className="navbar navbar-expand border-bottom shadow-sm px-4 justify-content-md-start justify-content-center">
-        <a href="#" className="navbar-brand fw-semibold m-0">
+        <a href="#" className="navbar-brand fw-bold m-0">
           <span className="custom-text-primary">Food</span>
           <span>Delivery</span>
         </a>
