bfebd185ca6b652f0a9a2bb58ef11dd4c7885770
Robin Hodge <mrrobinhodge@gmail.com>
2023-03-07 15:39:39 +0000

    The one and only commit....

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,12 @@
-import './App.scss';
+import './App.scss'
+import RestaurantsContainer from '../Main/RestaurantsContainer/RestaurantsContainer'
 
 function App() {
   return (
     <div className="App">
+    <RestaurantsContainer />
     </div>
-  );
+  )
 }
 
 export default App;
