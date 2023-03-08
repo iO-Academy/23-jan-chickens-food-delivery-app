66d4b01cbba55aa25d813b50cc74bfcb09c2e401
Bill Turvill <turvill@hotmail.co.uk>
2023-03-08 16:52:13 +0000

    various, increment, decrement

--- src/ItemCount/ItemCount.js
+++ src/ItemCount/ItemCount.js
@@ -14,21 +14,46 @@ const countItems = () => {
 
     // const orderVariable [orderVariable, setOrderVariable] = useState([{}]);
 
-    let orderVariable = menu.foodItems.map((item) => {
+    let orderVariableInitialState = menu.foodItems.map((item) => {
         let orderItem = {}
         orderItem.name = item.foodName
         orderItem.price = item.price
         orderItem.qty = 0
     })
 
-    const 
+    const orderVariable [orderVariable, setOrderVariable] = useState(orderVariableInitialState)
 
-    const handleClick = () => {
-        setOrderVariable(
-            orderVariable.concat()
-            
-            )
-    } 
+    const incrementQuantity = (indexOfItem) => {
+    // if item (object) = id of button
+
+        let incrementOrderVariable = orderVariable.map((item, index) => {
+            if(index == indexOfItem) {
+                return {...item, qty: item.qty+1}
+            } else {
+                return item    
+            }
+        })
+    }
+}
+
+const decrementQuantity = (indexOfItem) => {
+
+    let decrementOrderVariable = orderVariable.map((item, index) => {
+
+        if(item.qty >= 1) {
+            if(index == indexOfItem) {
+                return {...item, qty: item.qty+1}
+            } else {
+            return item    
+            }
+        }
+    })
+}
+    
+
+    // const handleClickIncrement = () => {
+    //     setOrderVariable(incrementQu)
+    // } 
 
     return (
 
@@ -39,7 +64,13 @@ const countItems = () => {
     )
 }
 
-
+// {
+//     "items": [
+//         {"name": "example", "price": 12.49, "qty": 1},
+//         {"name": "example 2", "price": 12.30, "qty": 1}
+//     ],
+//     "total": 24.79
+// }
 
 
 // Story 3 task 1 : make buttons that make number go up and down on click, display number, adds items to an order variable (useState) - make variable an object that we can use for POST request later
