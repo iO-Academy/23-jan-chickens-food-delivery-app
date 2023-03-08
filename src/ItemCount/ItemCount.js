484838c8ff10e9021bafa8663e2f7ce2b7bfbfb9
Bill Turvill <turvill@hotmail.co.uk>
2023-03-08 15:05:42 +0000

    initial

---
+++ src/ItemCount/ItemCount.js
@@ -0,0 +1,43 @@
+
+
+//insert into existing .map
+
+//increment function for plus buttons/decrement for minus
+
+//value must be stored in relation to dish 
+
+//orderVariable[itemid] ???  useSet
+
+//
+
+const countItems = () => {
+
+    const orderVariable [orderVariable, setOrderVariable] = useState([{}]);
+
+    const handleClick = () => {
+        setOrderVariable(
+            orderVariable.concat()
+            
+            )
+    } 
+
+    return (
+
+        <button className="increment" onClick={}></button>//increment
+        {count.id}
+        <button className="decrement" onClick={}></button>//decrement
+
+    )
+}
+
+
+
+
+// Story 3 task 1 : make buttons that make number go up and down on click, display number, adds items to an order variable (useState) - make variable an object that we can use for POST request later
+
+
+// counter component needs 2 props - number & setValue
+// starts with an empty array of objects (order variable)
+// order variable needs to go in main
+// .map menu into the orders.items
+// starts with quantity 0
