dfa69a9391da57fef016bb9724f7a4b716e6857b
iO Academy <hamyas@gmail.com>
2023-03-08 16:50:35 +0000

    removed redundant div

--- src/RestaurantsContainer/RestaurantsContainer.js
+++ src/RestaurantsContainer/RestaurantsContainer.js
@@ -18,13 +18,11 @@ const RestaurantsContainer = () => {
         <div className="container-fluid">
             <div className="row">
                 {restaurants?.map(restaurant =>
-                    <div key={restaurant.id} className="col-sm-12 col-md-6 col-lg-4 col-xl-3">
-                        <div className="py-2">
-                            <button className="restaurantButton btn btn-outline-primary btn-lg py-4 w-100" data-restaurant-id={restaurant.id}>{restaurant.name}</button>
-                        </div>
+                    <div key={restaurant.id} className="col-sm-12 col-md-6 col-lg-4 col-xl-3 py-2">
+                        <button className="restaurantButton btn btn-outline-primary btn-lg py-4 w-100" data-restaurant-id={restaurant.id}>{restaurant.name}</button>
                     </div>
-                ) ?? 
-                <p>Loading...</p>
+                ) ??
+                    <p>Loading...</p>
                 }
             </div>
         </div>
