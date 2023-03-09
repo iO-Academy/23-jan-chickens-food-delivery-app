f9b823a6ae1015f11824b12dfa057d64ae46178a
Robin Hodge <mrrobinhodge@gmail.com>
2023-03-09 11:48:03 +0000

    Fully responsive order bar, correct padding etc.

--- src/OrderSideBar/OrderSideBar.js
+++ src/OrderSideBar/OrderSideBar.js
@@ -37,12 +37,12 @@ const OrderSideBar = (props) => {
 
     return (
         <div className="container-fluid">
-            <div className="row">
+            <div className="row px-3">
                 <div className="col-sm-12 col-lg-9"></div>
-                <div className="card col-sm-12 col-lg-3 border-0 bg-light p-0">
+                <div className="card col-sm-12 col-lg-3 border-0 bg-light p-0 =">
                     <div className="card-body p-0">
-                        <div className="card-title text-start p-2">
-                            <i className="cart-logo fa-solid fa-cart-shopping ps-1"></i>
+                        <div className="card-title text-start container">
+                            <i className="cart-logo fa-solid fa-cart-shopping pt-3"></i>
                             <h3 className="order-heading ps-1 fs-5">Order</h3>
                         </div>
                         <div className="order-items text-start container">
@@ -51,7 +51,7 @@ const OrderSideBar = (props) => {
                             )
                                 .map((item, index) => {
                                     return (
-                                        // the span below is a temprary placeholder for the counter component
+                                        // the span below is a temporary placeholder for the counter component
                                         <div key={index} className="row">
                                             <p className="fw-bold col-7 ps-3">{item.name}</p>
                                             <span className="d-inline text-end col-5"> + {item.qty} - </span> 
