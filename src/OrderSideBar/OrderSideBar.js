a674d01f84e8b795a6056878ee6de4582d92f650
Robin Hodge <mrrobinhodge@gmail.com>
2023-03-09 13:28:30 +0000

    Better dynamic changes

--- src/OrderSideBar/OrderSideBar.js
+++ src/OrderSideBar/OrderSideBar.js
@@ -7,8 +7,8 @@ const OrderSideBar = (props) => {
     return (
         <div className="container-fluid">
             <div className="row px-3">
-                <div className="col-sm-12 col-lg-9"></div>
-                <div className="card col-sm-12 col-lg-3 border-0 bg-light p-0 =">
+                <div className="col-sm-12 col-lg-9 col-xl-10"></div>
+                <div className="card col-sm-12 col-lg-3 col-xl-2 border-0 bg-light p-0 =">
                     <div className="card-body p-0">
                         <div className="card-title text-start container">
                             <i className="cart-logo fa-solid fa-cart-shopping pt-3"></i>
@@ -23,8 +23,8 @@ const OrderSideBar = (props) => {
                                 return (
                                     // the span below is a temporary placeholder for the counter component
                                     <div key={index} className="row">
-                                        <p className="fw-bold col-7 ps-3">{item.name}</p>
-                                        <span className="d-inline text-end col-5"> + {item.qty} - </span>
+                                        <p className="fw-bold col-8 ps-3">{item.name}</p>
+                                        <span className="d-inline text-end col-4"> + {item.qty} - </span>
                                     </div>
                                 )
                             }
